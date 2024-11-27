7c175288b3bc6b9c5dc47c85f3abb1baa15ac557
Pablo <pablopierry98@gmail.com>
2024-11-27 16:54:14 -0300

    feat: add window logic

---
+++ controllers/menu.py
@@ -0,0 +1,27 @@
+import customtkinter as ctk
+
+class Menu(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Menu")
+        self.protocol("WM_DELETE_WINDOW", self.close_window)
+        self.geometry("600x400+50+50")
+        self.resizable(False, False)
+        
+        # Theme configuration
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
+
+    # Create the container
+    def create_container(self):
+        # Top Frame (por exemplo, pode conter entradas e botões principais)
+        self.top_frame = ctk.CTkFrame(self)
+        self.top_frame.pack(fill=ctk.X, pady=5, padx=5)
+        
+        # Mid Frame (pode conter listboxes, labels, etc.)
+        self.mid_frame = ctk.CTkFrame(self)
+        self.mid_frame.pack(fill=ctk.BOTH, expand=True)
+        
+        # Bottom Frame (pode conter botões de ações)
+        self.bottom_frame = ctk.CTkFrame(self)
+        self.bottom_frame.pack(fill=ctk.X, pady=5, padx=5)
