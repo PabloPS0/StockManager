dfe784464b518728ff7944d449297741fd31909c
Pablo <pablopierry98@gmail.com>
2024-11-30 03:24:15 -0300

    chore: non-significant changes in the window execution instance

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 import sys, os
 # Add the parent directory of the project to the system path to allow importing modules from it
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from controllers.lobby import StockManager
+from controllers.lobby import StockManager  # A classe do lobby principal
 
-# Create an instance of the StockManagerApp class and start the application
-if __name__ == "__main__":
+def main():
+    # Inicializa o Lobby como ponto de partida da aplicação
     app = StockManager()
     app.mainloop()
+
+if __name__ == "__main__":
+    main()
