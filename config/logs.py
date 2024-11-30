7bb7127116930cfadb117ec5119c4d3a5209ffe3
Pablo <pablopierry98@gmail.com>
2024-11-30 03:21:53 -0300

    feat: add new files

---
+++ config/logs.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+# Caminho para o arquivo de log
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'logs')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+log_file = os.path.join(log_dir, 'app.log')
+
+# Configuração do logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename=log_file,
+    filemode='a'
+)
+
+# Agora, você pode usar logging em qualquer parte do projeto
